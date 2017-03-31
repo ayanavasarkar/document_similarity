d8eef5a40e952d5449e402740544f31592bbe6a7
ayanavasarkar <f2013206@dubai.bits-pilani.ac.in>
2017-04-01 00:49:31 +0530

    comaprison with each transcript file word

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import algorithm
 import operator
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 
 class Main:
 	def __init__(self, sample_file):
@@ -21,15 +23,29 @@ class Main:
 		self.keywords=self.keywords[0:self.n]
 		return self.keywords
 
+	def script_keys_to_list(self,keywords,n):
+		script_list=[]
+		for i in range(n):
+			script_list.append(keywords[i][0])
+		return script_list
+
+
 	def transcript_preprocess(self,transcript):
-		self.stop_word_t = algorithm.Algorithm("SmartStoplist.txt", 4, 3, 3)
+		self.stop_word_t = algorithm.Algorithm("SmartStoplist.txt", 4, 1, 3)
 		self.transcript=open(transcript,'r')
 		self.t_text=self.transcript.read()
 
 		self.t_keywords = self.stop_word_t.run(self.t_text)				#first find the keywords
 		self.t_keywords= sorted(self.t_keywords,key=lambda l:l[1], reverse=True)
 		return self.t_keywords
-
+      
+      	def similarity(self,text1,text2):
+          	tfidf = vectorizer.fit_transform([text1, text2])
+          	x=((tfidf * tfidf.T).A)[0,1]
+		self.test(x)
+		return x
+	def test(self,x):
+		print x
 
 sample_file = "script.txt"
 
@@ -39,13 +55,26 @@ keywords=k.ret_keywords()
 n=10											#n= number of keywords to be taken into account
 keywords=k.truncated_keyword(n)
 #print len(keywords)
-#print "Keywords:", keywords
+#print keywords
+script_list=k.script_keys_to_list(keywords,n)
+print script_list
 
 
-print keywords
+files=['transcript_1.txt','transcript_2.txt','transcript_3.txt']
 
 transcript1="transcript_1.txt"
 
 t1_keywords=k.transcript_preprocess(transcript1)
+trans1_list=k.script_keys_to_list(t1_keywords,len(t1_keywords))
+#print trans1_list
 
-print t1_keywords
+vectorizer = TfidfVectorizer( stop_words='english')
+key_score=[]
+for i in range(n):
+    tmp=0
+    #key_score[i][0].append(script_list[i])
+    for j in range(len(trans1_list)):
+        tmp=tmp+k.similarity(script_list[i],trans1_list[j])
+    key_score.append(tmp)
+
+print key_score
