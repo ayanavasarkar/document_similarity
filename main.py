cd24434b9b31052e5b60a059159539ff80f07f5f
Ayanava Sarkar <fdai5168@pc04.informatik.hs-fulda.de>
2017-03-31 11:42:32 +0200

    extracting keywords from the transcripts

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ class Main:
 		
 			
 	def stop_word_removal(self):
-		self.rake_object = algorithm.Algorithm("SmartStoplist.txt", 4, 3, 3)	#stop word removal using the smart stopword list
+		self.stop_word = algorithm.Algorithm("SmartStoplist.txt", 4, 3, 3)	#stop word removal using the smart stopword list
 	
 	def ret_keywords(self):
-		self.keywords = self.rake_object.run(self.text)				#first find the keywords
+		self.keywords = self.stop_word.run(self.text)				#first find the keywords
 		self.keywords= sorted(self.keywords,key=lambda l:l[1], reverse=True)	#sort the keywords in descending order of score
 		return self.keywords
 
@@ -20,7 +20,17 @@ class Main:
 		self.n=n
 		self.keywords=self.keywords[0:self.n]
 		return self.keywords
-		
+
+	def transcript_preprocess(self,transcript):
+		self.stop_word_t = algorithm.Algorithm("SmartStoplist.txt", 4, 3, 3)
+		self.transcript=open(transcript,'r')
+		self.t_text=self.transcript.read()
+
+		self.t_keywords = self.stop_word_t.run(self.t_text)				#first find the keywords
+		self.t_keywords= sorted(self.t_keywords,key=lambda l:l[1], reverse=True)
+		return self.t_keywords
+
+
 sample_file = "script.txt"
 
 k=Main(sample_file)
@@ -34,3 +44,8 @@ keywords=k.truncated_keyword(n)
 
 print keywords
 
+transcript1="transcript_1.txt"
+
+t1_keywords=k.transcript_preprocess(transcript1)
+
+print t1_keywords
